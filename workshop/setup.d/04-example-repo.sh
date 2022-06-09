1c9d38cf70610d08cb5783034c53517411bc274f
Corby Page <pcorby@vmware.com>
2022-06-09 15:23:34 -0500

    Update 04-example-repo.sh

--- workshop/setup.d/04-example-repo.sh
+++ workshop/setup.d/04-example-repo.sh
@@ -8,6 +8,6 @@ set +e
 #     - replace hard-coded registry name with session registry
 git clone https://github.com/ciberkleid/cartographer-concepts.git $HOME/intro-temp
 mkdir -p $HOME/intro
-mv $HOME/intro-temp/layout-2/* $HOME/cartographer-concepts
+mv $HOME/intro-temp/layout-2/* $HOME/intro
 find ./intro -type f -not -path '*/\.*' -print0 | xargs -0 sed -i "s/harbor\.tanzu\.coraiberkleid\.site\/carto-demo/$REGISTRY_HOST/g"
 rm -rf $HOME/intro-temp
