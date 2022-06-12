f5d77626f9833a74e72fb91f913678904932f1c4
Cora Iberkleid <ciberkleid@gmail.com>
2022-06-12 12:57:55 -0400

    dynamically create exercises folder

--- workshop/setup.d/04-example-repo.sh
+++ workshop/setup.d/04-example-repo.sh
@@ -2,12 +2,21 @@
 set -x
 set +e
 
+ROOT=$HOME/exercises
+
 # Set up sample files
 #     - clone repo
-#     - copy files of interest to user $HOME/intro directory
+#     - copy files of interest to user $ROOT/intro directory
 #     - replace hard-coded registry name with session registry
-git clone https://github.com/ciberkleid/cartographer-concepts.git $HOME/intro-temp
-mkdir -p $HOME/intro
-mv $HOME/intro-temp/layout-2/* $HOME/intro
+git clone https://github.com/ciberkleid/cartographer-concepts.git $ROOT/intro-temp
+mkdir -p $ROOT/intro
+mv $ROOT/intro-temp/layout-2/* $ROOT/intro
 find ./intro -type f -not -path '*/\.*' -print0 | xargs -0 sed -i "s/harbor\.tanzu\.coraiberkleid\.site\/carto-demo/$REGISTRY_HOST/g"
-rm -rf $HOME/intro-temp
+rm -rf $ROOT/intro-temp
+
+# Download cartographer repo examples
+git clone https://github.com/vmware-tanzu/cartographer.git $ROOT/examples-temp
+mkdir -p $ROOT/examples
+mv $ROOT/examples-temp/examples/gitwriter-sc $ROOT/examples
+mv $ROOT/examples-temp/examples/basic-delivery $ROOT/examples
+rm -rf $ROOT/examples-temp
