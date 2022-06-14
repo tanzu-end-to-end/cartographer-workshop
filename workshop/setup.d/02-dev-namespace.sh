f7773f34a6411507b849978f9a53e63a3c867cba
pcorby <pcorby@vmware.com>
2022-06-14 11:05:36 -0500

    Testing script

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -5,4 +5,4 @@ set +e
 kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "learningcenter-registry-credentials"}]}'
 kubectl patch serviceaccount default -p '{"secrets": [{"name": "learningcenter-registry-credentials"}]}'
 
-envsubst < intro/01_manual/image.yaml > image-tmp.yaml && mv image-tmp.yaml intro/01_manual/image.yaml
+envsubst < exercises/intro/01_manual/image.yaml > /tmp/image.yaml && mv /tmp/image.yaml exercises/intro/01_manual/image.yaml
