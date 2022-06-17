2fe0b70fb8a8c11257b58c8c5e39524d450d2254
Cora Iberkleid <ciberkleid@gmail.com>
2022-06-17 16:31:35 -0400

    cleanup

--- workshop/setup.d/04-example-repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set +e
-
-cd $HOME/exercises
-
-echo "#@data/values" > ./values.yaml
-echo "---" >> ./values.yaml
-pathEnv=".image_prefix"  valueEnv=$( echo "${REGISTRY_HOST}/" ) yq -i 'eval(strenv(pathEnv)) |= strenv(valueEnv)' ./values.yaml
