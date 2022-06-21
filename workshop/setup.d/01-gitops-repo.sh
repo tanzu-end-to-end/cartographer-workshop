d1fd6654351ce4cf820f2981301cb7ab73852310
pcorby <pcorby@vmware.com>
2022-06-21 14:53:18 -0500

    Authoring GitOps content

---
+++ workshop/setup.d/01-gitops-repo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+set +e
+
+export REPO_NAME=$SESSION_NAMESPACE-$(date +%m%d)
+
+mkdir $REPO_NAME
+cd $REPO_NAME
+echo "# Cartographer Delivery Repo" >> README.MD
+git init
+git checkout -b main
+git config user.name gitea_admin
+git config user.email "gitea_admin@example.com"
+git add .
+git commit -a -m "Initial Commit"
+
+git remote add origin https://gitea_admin:$GITEA_PASSWORD@gitea.${INGRESS_DOMAIN}/gitea_admin/$REPO_NAME.git
+git push -u origin main
