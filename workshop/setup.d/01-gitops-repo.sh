764e80e5df68910d498b9943aa1941aa9dbc44a2
Chris DeLashmutt <cdelashmutt@pivotal.io>
2022-08-01 12:19:10 -0400

    Adding in parameterization for gitea

--- workshop/setup.d/01-gitops-repo.sh
+++ workshop/setup.d/01-gitops-repo.sh
@@ -7,10 +7,10 @@ cd $SESSION_NAMESPACE
 echo "# Cartographer Delivery Repo" >> README.MD
 git init
 git checkout -b main
-git config user.name gitea_admin
-git config user.email "gitea_admin@example.com"
+git config user.name ${GITEA_USER}
+git config user.email "${GITEA_USER}@example.com"
 git add .
 git commit -a -m "Initial Commit"
 
-git remote add origin https://gitea_admin:VMware1\!@gitea.${INGRESS_DOMAIN}/gitea_admin/$SESSION_NAMESPACE.git
+git remote add origin https://${GITEA_USER}:${GITEA_PASSWORD}@gitea.${INGRESS_DOMAIN}/gitea_admin/$SESSION_NAMESPACE.git
 git push -u origin main
