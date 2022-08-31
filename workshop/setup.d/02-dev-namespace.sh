09ee13b64c2fd9ab2751893ac10d551cda35a81f
Chris DeLashmutt <cdelashmutt@pivotal.io>
2022-08-31 18:48:53 -0400

    Isolate workshop and session "cluster" resources

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -2,18 +2,25 @@
 set -x
 set +e
 
+function replace_env_vars() {
+  temp_filename = /tmp/$(basename $1)
+  envsubst < $1 > $temp_filename && mv $temp_filename $1
+}
+
 kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "learningcenter-registry-credentials"}]}'
 kubectl patch serviceaccount default -p '{"secrets": [{"name": "learningcenter-registry-credentials"}]}'
 
 export SOURCE_REFERENCE="\${NEW_SOURCE}"
-envsubst < exercises/manual/image.yaml > /tmp/image.yaml && mv /tmp/image.yaml exercises/manual/image.yaml
-envsubst < exercises/examples/gitwriter-sc/app-operator/template-git-writer.yaml > /tmp/template-git-writer.yaml && mv /tmp/template-git-writer.yaml exercises/examples/gitwriter-sc/app-operator/template-git-writer.yaml
+for file in examples/basic-delivery/app-operator/*.yaml examples/gitwriter-sc/app-operator/*.yaml; do 
+  replace_env_vars $file 
+done
 
-kubectl apply -f exercises/examples/gitwriter-sc/app-operator/config-service.yaml
-kubectl apply -f exercises/examples/gitwriter-sc/app-operator/git-writer-task.yaml
-kubectl apply -f exercises/examples/gitwriter-sc/app-operator/runtemplate-tekton-gitwriter.yaml
-kubectl apply -f exercises/examples/gitwriter-sc/app-operator/template-git-writer.yaml
+#kubectl apply -f exercises/examples/gitwriter-sc/app-operator/config-service.yaml
+#kubectl apply -f exercises/examples/gitwriter-sc/app-operator/git-writer-task.yaml
+#kubectl apply -f exercises/examples/gitwriter-sc/app-operator/runtemplate-tekton-gitwriter.yaml
+#kubectl apply -f exercises/examples/gitwriter-sc/app-operator/template-git-writer.yaml
+kubectl patch clustertemplate $SESSION_NAMESPACE-git-writer --type=json -p="[{'op': 'replace', 'path': '/spec/params/0/default', 'value': 'https://${GITEA_USER}:${GITEA_PASSWORD}@${GITEA_DOMAIN}/${GITEA_USER}/osscon-deliveries.git'}]"
 
-kubectl apply -f exercises/examples/basic-delivery/app-operator/delivery.yaml
-kubectl apply -f exercises/examples/basic-delivery/app-operator/deploy-app.yaml
-kubectl apply -f exercises/examples/basic-delivery/app-operator/source-git-repository.yaml
+#kubectl apply -f exercises/examples/basic-delivery/app-operator/delivery.yaml
+#kubectl apply -f exercises/examples/basic-delivery/app-operator/deploy-app.yaml
+#kubectl apply -f exercises/examples/basic-delivery/app-operator/source-git-repository.yaml
